41a2ba2d8e51598084a393e2174b16368c72bb92
Luana Maria <luana_dark34@hotmail.com>
2024-05-01 00:09:32 -0300

    estado final

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Modelo 1", "123443422", 90);
+nokia.Ligar();
+nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("12332212", "Modelo 2", "123456789", 160);
+iphone.Ligar();
+iphone.InstalarAplicativo("Youtube");
